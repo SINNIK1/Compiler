c9332559b717b261828fc72a205917e5690bf5a7
SINNIK1 <k9811699955@gmail.com>
2025-11-13 23:32:46 +0530

    Shit Thrusday - No work- make some minor changes

--- src/driver.cpp
+++ src/driver.cpp
@@ -3,6 +3,7 @@
 #include<string>
 #include "frontend/lexer.h"
 #include "frontend/parser.h"
+#include "frontend/sema.h"
 #include "../common/diag.h"
 
 using namespace std;
@@ -25,5 +26,19 @@ int main(int argc, char** argv){
     tinyc::DiagEngine diags;
     tinyc::Lexer lexer(src, diags);
     auto tokens = lexer.tokenize();
+
+    tinyc::Parser parser(tokens, diags);
+    auto program = parser.parse();
+
+    tinyc::Sema sema(diags);
+    sema.analyze(program.get());
+
+    if(diags.hadErrors()){
+        diags.print();
+        return 1;
+    }
+
+    cout<<"Parse + semantic analysis succeeded.\n";
+    return 0;
 
 }
